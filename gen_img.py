6e3de1cfe31fc1ad27a095a0a47879e11af12aa4
Varsha02nats <varsha02nats@gmail.com>
2024-08-20 13:30:53 -0400

    image generator updated

---
+++ gen_img.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Unable to read camera feed")
+
+#output_dir = 'output_images'
+output_dir = 'output_images1'
+os.makedirs(output_dir, exist_ok=True)
+
+img_counter = 0
+
+while True:
+    ret, frame = cap.read()
+
+    if not  ret:
+        break
+
+    cv2.imshow('Webcam', frame)
+
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        print("Escape hit, closing...")
+        break
+    elif k%256 == ord('s'): # press 's' key to save the frame
+        img_name = os.path.join(output_dir,"opencv_frame_{}.png".format(img_counter))
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+
+cap.release()
+cv2.destroyAllWindows()
